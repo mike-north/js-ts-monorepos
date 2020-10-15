4dea052c8bc62039e478b82ae71b1f9ccb7ba918
Mike North <michael.l.north@gmail.com>
2020-10-14 20:10:01 -0700

    01-yarn-workspaces notes

---
+++ COURSE_FILES/01-yarn-workspaces/utils/src/http-error.ts
@@ -0,0 +1,42 @@
+/**
+ * High-level outcome of an HTTP status code
+ * @public
+ */
+export enum HTTPErrorKind {
+  Information = 100,
+  Success = 200,
+  Redirect = 300,
+  Client = 400,
+  Server = 500,
+}
+
+/**
+ * Determine the high-level outcome of a HTTP status code
+ *
+ * @param status - http status code
+ * @see HTTPErrorKind
+ * @public
+ */
+function determineKind(status: number): HTTPErrorKind {
+  if (status >= 100 && status < 200) return HTTPErrorKind.Information;
+  else if (status < 300) return HTTPErrorKind.Success;
+  else if (status < 400) return HTTPErrorKind.Redirect;
+  else if (status < 500) return HTTPErrorKind.Client;
+  else if (status < 600) return HTTPErrorKind.Server;
+  else throw new Error(`Unknown HTTP status code ${status}`);
+}
+
+/**
+ * An error that's associated with a HTTP response status code
+ * @public
+ */
+export default class HTTPError extends Error {
+  kind: HTTPErrorKind;
+
+  constructor(info: { statusText: string; status: number }, message: string) {
+    super(
+      `HTTPError [status: ${info.statusText} (${info.status})]\n${message}`
+    );
+    this.kind = determineKind(info.status);
+  }
+}
