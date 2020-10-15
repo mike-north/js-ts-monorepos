fd6c349a992a0cbb291be4a4ee6771eafda04889
Mike North <michael.l.north@gmail.com>
2020-10-15 09:08:49 -0700

    first break

--- packages/utils/src/deferred.ts
+++ packages/utils/src/deferred.ts
@@ -14,23 +14,23 @@ export type RejectHandler = (reason: unknown) => void;
  * @public
  */
 class Deferred<T> {
-  #_promise: Promise<T>;
-  #_resolve!: ResolveHandler<T>;
-  #_reject!: RejectHandler;
+  _promise: Promise<T>;
+  _resolve!: ResolveHandler<T>;
+  _reject!: RejectHandler;
   constructor() {
-    this.#_promise = new Promise((resolve, reject) => {
-      this.#_resolve = resolve;
-      this.#_reject = reject;
+    this._promise = new Promise((resolve, reject) => {
+      this._resolve = resolve;
+      this._reject = reject;
     });
   }
   get promise(): Promise<T> {
-    return this.#_promise;
+    return this._promise;
   }
   get resolve(): ResolveHandler<T> {
-    return this.#_resolve;
+    return this._resolve;
   }
   get reject(): RejectHandler {
-    return this.#_reject;
+    return this._reject;
   }
 }
 export default Deferred;
