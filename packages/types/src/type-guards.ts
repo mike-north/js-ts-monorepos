a36fe9a8569a0ec1e8a93fc7f528ce3b41d8d8a8
Mike North <michael.l.north@gmail.com>
2020-10-14 20:37:44 -0700

    04-linting

--- packages/types/src/type-guards.ts
+++ packages/types/src/type-guards.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { IChannel, IMessage, ITeam } from "./types";
 
