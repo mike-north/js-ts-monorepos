59e955509c88cff2b2e56237da7ea4d4bf9a8944
Mike North <michael.l.north@gmail.com>
2020-10-14 22:11:13 -0700

    feat(ui): a SPA GUI for shlack app

---
+++ packages/ui/src/components/SelectedChannel.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { match } from "react-router";
+import { IChannel } from "@shlack/types";
+import Channel from "./Channel";
+
+const SelectedChannel: React.FunctionComponent<{
+  match: match<{ channelId: string }>;
+  channels: IChannel[];
+}> = ({ match, channels }) => {
+  if (!channels) throw new Error("no channels");
+  if (!match) throw new Error("no match");
+
+  const { params } = match;
+  if (!match) return <p>No match params</p>;
+  const { channelId: selectedChannelId } = params;
+  if (!selectedChannelId) return <p>Invalid channelId</p>;
+  const selectedChannel = channels.find(
+    (c: IChannel) => c.id === selectedChannelId
+  );
+  if (!selectedChannel)
+    return (
+      <div>
+        <p>Could not find channel with id {selectedChannelId}</p>
+        <pre>{JSON.stringify(channels, null, "  ")}</pre>
+      </div>
+    );
+
+  return <Channel channel={selectedChannel} />;
+};
+
+export default SelectedChannel;
