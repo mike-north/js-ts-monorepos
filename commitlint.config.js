8098f15cbb8a2846a469b127c71cfd55cf8ca36e
Mike North <michael.l.north@gmail.com>
2020-10-15 11:52:35 -0700

    fix(utils): correction to commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ["@commitlint/config-lerna-scopes"] };
+module.exports = {
+  extends: [
+    "@commitlint/config-conventional",
+    "@commitlint/config-lerna-scopes",
+  ],
+};
