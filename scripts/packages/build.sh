e12fee4a4afe4fe8131e14b0ae6874dab22ef223
Mike North <michael.l.north@gmail.com>
2020-10-14 21:03:43 -0700

    06-scripty

---
+++ scripts/packages/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "┏━━━ 📦 Building $(pwd) ━━━━━━━━━━━━━━━━━━━"
+yarn tsc -b
---
+++ scripts/packages/lint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "┏━━━ 🕵️‍♀️ LINT: eslint src --ext ts,js,tsx,jsx ━━━━━━━"
+yarn eslint src --ext ts,js,tsx,jsx
