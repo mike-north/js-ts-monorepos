b95793cf852e4f28e70feb9ffc625767ae389776
Mike North <michael.l.north@gmail.com>
2020-10-14 20:10:01 -0700

    01-yarn-workspaces notes

---
+++ COURSE_FILES/01-yarn-workspaces/utils/src/api.ts
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import Deferred from "./deferred";
+
+/**
+ *
+ * @param  getData
+ * @param  options
+ */
+export function useAsyncDataEffect<T>(
+  getData: () => Promise<T>,
+  options: {
+    stateName: string;
+    otherStatesToMonitor?: unknown[];
+    setter: (arg: T) => void;
+  }
+): void {
+  let cancelled = false;
+  const { setter, stateName } = options;
+  useEffect(() => {
+    const d = new Deferred<T>();
+
+    getData()
+      .then((jsonData) => {
+        if (cancelled) return;
+        else d.resolve(jsonData);
+      })
+      .catch(d.reject);
+
+    d.promise
+      .then((data) => {
+        if (!cancelled) {
+          console.info(
+            "%c Updating state: " + stateName,
+            "background: green; color: white; display: block;"
+          );
+          setter(data);
+        }
+      })
+      .catch(console.error);
+    return () => {
+      cancelled = true;
+    };
+  }, [...(options.otherStatesToMonitor || []), stateName]);
+}
---
+++ COURSE_FILES/01-yarn-workspaces/utils/src/networking.ts
@@ -0,0 +1,57 @@
+import { stringifyError } from "./error";
+import HTTPError from "./http-error";
+
+/**
+ * Make a GET request, and decode the response body as JSON
+ *
+ * @param input - request info
+ * @param init - request options
+ * @internal
+ */
+async function getJSON(input: RequestInfo, init?: RequestInit) {
+  try {
+    const response = await fetch(input, init);
+    const responseJSON = await response.json();
+    return { response, json: responseJSON };
+  } catch (err) {
+    throw new Error(
+      stringifyError(
+        `Networking/getJSON: An error was encountered while fetching ${JSON.stringify(
+          input
+        )}`,
+        err
+      )
+    );
+  }
+}
+
+/**
+ * Make a same-origin GET request to the API
+ *
+ * @param path - API path
+ * @param init - fetch options
+ * @public
+ */
+export async function apiCall(
+  path: string,
+  init?: RequestInit
+): Promise<unknown> {
+  let response;
+  let json;
+  try {
+    const jsonRespInfo = await getJSON(`/api/${path}`, init);
+    response = jsonRespInfo.response;
+    json = jsonRespInfo.json;
+  } catch (err) {
+    if (err instanceof HTTPError) throw err;
+    throw new Error(
+      stringifyError(
+        `Networking/apiCall: An error was encountered while making api call to ${path}`,
+        err
+      )
+    );
+  }
+  if (!response.ok)
+    throw new HTTPError(response, "Problem while making API call");
+  return json;
+}
